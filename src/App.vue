15b5a1c2d2f950c4f416f5bd3de61e655ec4dda4
Hnooz <mohammededris23@gmail.com>
2020-06-16 02:49:50 +0200

    makeing nav and overview

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div id="app">
+    <Nav/>
+
+    <Overview/>
+  </div>
+</template>
+
+<script>
+
+import Nav from './components/Nav'
+import Overview from './components/Overview'
+export default {
+  name: 'App',
+  components:{
+    Nav,
+    Overview
+  }
+}
+</script>
+
+<style src="./assets/css/tailwind.css">
+
+</style>
+
+
+
+
---
+++ src/components/Overview.vue
@@ -0,0 +1,47 @@
+<template>
+    <section id="overview" class="container border-b-2 border-teal-500  mb-12 mt-12 mx-auto px-12">
+        <div class="my-16">
+            <h1 class="font-semibold mb-2 text-2xl text-center text-teal-500">About Afia</h1>
+            <p class="font-medium md:text-xl text-center text-gray-700 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id perspiciatis repellat eius si
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id perspiciatis repellat eius si
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id perspiciatis repellat eius si
+            </p>
+        </div>
+        <div class="md:flex items-center justify-between my-16">
+            <div class="mb-10 md:mb-0 border-b-2 border-teal-500 shadow-2xl mx-10">
+
+                <div class="flex px-1">
+                    <svg class="h-6 w-5 text-teal-500 mr-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+                    <h1 class="font-mono text-teal-500 text-xl">Who We Are</h1>
+                </div>
+                
+                <p class="font-light p-1 text-gray-700 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id perspiciatis repellat eius si</p>
+            </div>
+
+            <div class="mb-10 md:mb-0 border-b-2 border-teal-500 shadow-2xl mx-10">
+                <div class="flex px-1">
+                    <svg class="h-6 w-5 text-teal-500 mr-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                   <h1 class="font-mono text-teal-500 text-xl">Our Mession</h1> 
+                </div>
+                
+                <p class="font-light p-1 text-gray-700 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id perspiciatis repellat eius si</p>
+            </div>
+
+            <div class="mb-10 md:mb-0 border-b-2 border-teal-500 shadow-2xl mx-10">
+                <div class="flex px-1">
+                    <svg class="h-6 w-5 text-teal-500 mr-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+                    <h1 class="font-mono text-teal-500 text-xl">Our Vision</h1>
+                </div>
+                
+                <p class="font-light p-1 text-gray-700 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id perspiciatis repellat eius si</p>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name:'Overview'
+}
+</script>
