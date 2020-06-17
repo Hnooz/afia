da5650a6e382c22f77ffe7e163adb5688978b8c5
Hnooz <mohammededris23@gmail.com>
2020-06-17 17:57:17 +0200

    wip

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -41,8 +41,8 @@
                         <p class="mb-6 text-sm font-medium text-gray-700 md:max-w-md text-center  md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt commodi ab ipsum numquam at labore nobis aliquam quod tempore ,</p>
                     </div>
 
-                    <div class="max-w-sm md:max-w-none mt-10 mx-auto">
-                        <a href="#DownloadandShare" class="flex justify-center md:justify-start bg-teal-500 border-2 hover:bg-transparent hover:border-teal-400 hover:text-teal-400 md:mx-0 mx-16 px-2 py-3 rounded-lg text-white text-xl">Download</a>
+                    <div class="flex justify-center md:justify-start max-w-sm md:max-w-none mt-10 mx-auto">
+                        <a href="#DownloadandShare" class=" bg-teal-500 border-2 hover:bg-transparent hover:border-teal-400 hover:text-teal-400 md:mx-0 mx-16 px-2 py-3 rounded-lg text-white text-xl">Download</a>
                     </div>
 
             </div>
